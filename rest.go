e92fcbef41554459ed35c559c7eb6b570d1911bb
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-05-03 17:32:42 +1000

    Share db connection.

--- rest.go
+++ rest.go
@@ -10,10 +10,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 // Index controller
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	email := testDb()
-	fmt.Fprintf(w, "Welcome!\n%s", email)
+	var firstName string
+	err := db.QueryRow("SELECT first_name FROM customer WHERE customer_id = $1", 1).Scan(&firstName)
+	if err != nil {
+		fmt.Fprintf(w, "Database error\n")
+	} else {
+		fmt.Fprintf(w, "Welcome %s!\n", firstName)
+	}
 }
 
 // Hello controller
@@ -21,31 +28,19 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
-func testDb() string {
+func main() {
 	db, err := sql.Open("postgres", "host=0.0.0.0 user=postgres password=postgres port=5432 dbname=dvdrental sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
-	customerID := 1
-	rows, err := db.Query("SELECT email FROM customer WHERE customer_id = $1", customerID)
+	db.SetMaxIdleConns(100)
+
+	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
 
-	for rows.Next() {
-		var email string
-		if err := rows.Scan(&email); err != nil {
-			log.Fatal(err)
-		}
-		return email
-	}
-	return "error"
-}
-
-func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
