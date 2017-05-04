63b288bb317c0af587d7db07c77ea63e352ba43b
Glenn Ji <qiang.ji@yahoo.com.au>
2017-05-04 21:47:34 +1000

    refactor code.

--- rest.go
+++ rest.go
@@ -10,16 +10,41 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Staff model
+type Staff struct {
+	id        int
+	firstName string
+	lastName  string
+	addressID int
+	email     string
+	storeID   int
+	active    bool
+}
+
 var db *sql.DB
 
-// Index controller
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var firstName string
-	err := db.QueryRow("SELECT first_name FROM customer WHERE customer_id = $1", 1).Scan(&firstName)
+func staffsIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	rows, err := db.Query("SELECT staff_id, first_name, last_name, address_id, email, store_id, active FROM staff")
 	if err != nil {
-		fmt.Fprintf(w, "Database error\n")
-	} else {
-		fmt.Fprintf(w, "Welcome %s!\n", firstName)
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	staffs := make([]*Staff, 0)
+	for rows.Next() {
+		staff := new(Staff)
+		err := rows.Scan(&staff.id, &staff.firstName, &staff.lastName, &staff.addressID, &staff.email, &staff.storeID, &staff.active)
+		if err != nil {
+			log.Fatal(err)
+		}
+		staffs = append(staffs, staff)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, staff := range staffs {
+		fmt.Fprintf(w, "%d, %s, %s, %d, %s, %d, %t\n", staff.id, staff.firstName, staff.lastName, staff.addressID, staff.email, staff.storeID, staff.active)
 	}
 }
 
@@ -35,15 +60,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	db.SetMaxIdleConns(100)
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	router := httprouter.New()
-	router.GET("/", Index)
+	router.GET("/", staffsIndex)
 	router.GET("/hello/:name", Hello)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
